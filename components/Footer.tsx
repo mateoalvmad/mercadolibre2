e28e1e52800206750897c82d24691d40ff5982fc
arleyberrio <arleyberrioarroyave@gmail.com>
2023-04-15 16:20:06 -0500

    footer y prefooter completos

---
+++ components/Footer.tsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <div className="mt-0.5 grid pt-3 bg-white pl-36 pr-36">
+      <div className="flex gap-4 font-normal align-center mb-1 text-gray-950">
+          <span>Trabaja con nosotros</span>
+          <span>Términos y condiciones</span>
+          <span>Cómo cuidamos tu privacidad</span>
+          <span>Accesibilidad</span>
+          <span>Ayuda / PQR</span>
+          <span>www.sic.gov.co</span>
+      </div>
+      <span className="text-gray-500 font-light">Copyright © 1999-2023 MercadoLibre Colombia LTDA.</span>
+      <span className="text-gray-500 font-extralight">Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia</span>
+      <div className="h-0.5  border-1  mt-6 bg-gray-200" ></div>
+      <div className="flex gap-8 pt-6 pb-8 w-2/3">
+          <img src="/media/sic.png" alt="industria y comercio"/>
+          <img src="/media/pum.png" alt="pare y compare"/>
+      </div>                
+  </div>
+  )
+}
+
+export {Footer};
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors:{
+        'footer-text-black':'#333333',
+        'link-prefooter-blue':'#3483FA',
+        'background-index':'#666666',
+      }
+    },
+  },
+  plugins: [],
+}
