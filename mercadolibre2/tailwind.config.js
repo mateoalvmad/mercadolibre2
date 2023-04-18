8945012f7b5008850d9f577be9958680415d252a
mateoalvmad <mateoalvarez@MacBook-Pro-de-Mateo-Blu-3.local>
2023-04-18 12:34:40 -0500

    sections

--- mercadolibre2/tailwind.config.js
+++ mercadolibre2/tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './hooks/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.{js,ts,jsx,tsx}',
+    './context/**/*.{js,ts,jsx,tsx}',
+    './images/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -11,6 +16,13 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      spacing: {
+        'psections': '10.25rem',
+        'hcombo': '21.25rem',
+      },
+      colors: {
+        'mcgray': '#ebebeb',
       },
     },
   },
